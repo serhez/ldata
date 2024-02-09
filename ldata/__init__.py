aade19044eae17853f4f41006341a01b409b7bb3
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-02-09 14:40:31 +0200

    fix: include `Experiment` in module exported items

--- ldata/__init__.py
+++ ldata/__init__.py
@@ -1,7 +1,5 @@
 from .benchmark import Benchmark
 from .dataset import Dataset
+from .experiment import Experiment
 
-__all__ = [
-    "Benchmark",
-    "Dataset",
-]
+__all__ = ["Benchmark", "Dataset", "Experiment"]
