1b712611b5c91de8daf74a98c30cf25c85f3a30e
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-08-15 15:32:47 +0300

    feat(benchmark): `n_skip_samples` parameter in `evaluate_subject`

--- ldata/benchmark.py
+++ ldata/benchmark.py
@@ -129,6 +129,7 @@ class Benchmark(ABC, Dataset):
         self,
         subject: Callable[[list[Any]], tuple[list[Any], Addable | None]],
         n_samples: int | None = None,
+        n_skip_samples: int = 0,
         metric: EvaluationMetric = EvaluationMetric.EXACT,
         aggregation_fn: NumberListOperation = NumberListOperation.MEAN,
         instructed: bool = True,
@@ -150,14 +151,17 @@ class Benchmark(ABC, Dataset):
         ### Parameters
         ----------
         `subject`: the subject to evaluate, which must be a function that takes an array of input strings and returns a tuple containing an array of output strings and an optional addable object with extra information about the generation process.
-        `n_samples`: the number of samples to evaluate the subject on.
+        `n_samples`: the number of samples from the test set to evaluate the subject on.
         - If `None` (default), the whole test set is used.
+        `n_skip_samples`: the number of samples to skip from the beginning of the test set.
+        - The number of samples that will be used is `n_samples - n_skip_samples`. Thus, `n_skip_samples` must be strictly less than `n_samples`.
+        - If `shuffle == True`, this parameter is ignored.
         `metric`: the metric to evaluate the output.
         `aggregation_fn`: the method to aggregate the scores of the (input, output) pairs.
         `instructed`: whether to use the instructed test set (as given by `get_instructed`) or the regular test set.
         `shuffle`: whether to shuffle the test set before selecting the samples.
         - If `shuffle == False`, repeated calls with the same `n_samples` will test the given subject on the same samples (the first `n_samples` of the current test set).
-        - If `shuffle == True`, the test set will be shuffled before selecting the samples, hence the results will be different for each call.
+        - If `shuffle == True`, the test set will be shuffled before selecting the samples, hence the results will be different for each call. The parameter `n_skip_samples` is ignored in this case.
         - You can also shuffle the test set before calling this function via the `shuffle` method.
         `unsafe`: if `True`, the exceptions raised by the subject will not be caught, which might lead to the function crashing in the middle of the evaluation and all the results being lost.
         - The advantage of setting `unsafe = True` is that the samples can be passed to the subject in batches, which might be faster depending on the subject's implementation.
@@ -178,6 +182,7 @@ class Benchmark(ABC, Dataset):
         ----------
         `ValueError`: if `aggregation_fn` is not supported.
         `AssertionError`: if `n_samples` is not `None` and is less than 1, or greater than the number of samples in the test set.
+        `AssertionError`: if `n_skip_samples` is less than 0 or greater than the number of samples in the test set or than `n_samples`.
         `AssertionError`: if the number of inputs and targets is not the same.
         - This will happen if the child class' implementation is incorrect.
         `AssertionError`: if the number of output strings returned by the subject is not the same as the number of input strings.
@@ -193,6 +198,12 @@ class Benchmark(ABC, Dataset):
         assert (
             n_samples is None or self.test_len >= n_samples >= 1
         ), "n_samples must be >= 1 and <= len(test_set)."
+        assert (
+            n_samples is None or n_skip_samples < n_samples
+        ), "n_skip_samples must be less than n_samples."
+        assert (
+            0 <= n_skip_samples < self.test_len
+        ), "n_skip_samples must be >= 0 and < len(test_set)."
 
         if instructed:
             test_set = self.get_instructed()
@@ -203,7 +214,7 @@ class Benchmark(ABC, Dataset):
             if shuffle:
                 test_set = test_set.sample(n_samples)
             else:
-                test_set = test_set[:n_samples]
+                test_set = test_set[n_skip_samples:n_samples]
 
         inputs = test_set.inputs
         targets = test_set.targets
